c86a513bc720b443ec5c8a16f68fb58bbba2e6f9
running-poe <djunkovsky.p@gmail.com>
2026-03-09 15:48:13 +0300

    изменено имя переменной переменной окружения для бота

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,7 +2,7 @@ import Config
 
 # Читаем токен из переменной окружения.
 # Если переменная не задана, программа упадет с понятной ошибкой.
-token = System.get_env("BOT_TOKEN") || raise("BOT_TOKEN environment variable is not set!")
+token = System.get_env("TIME_BOT_TOKEN") || raise("TIME_BOT_TOKEN environment variable is not set!")
 
 # Передаем токен в настройки приложения
 config :time_bot, bot_token: token
