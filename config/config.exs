b2c5145cb5714948a50b8e7b56b56558a8f75f2e
running-poe <djunkovsky.p@gmail.com>
2026-03-09 15:32:48 +0300

    случайно токен заэкспозил, убрал

--- config/config.exs
+++ config/config.exs
@@ -2,10 +2,10 @@ import Config
 
 # Настройки вашего бота
 config :time_bot,
-  bot_token: "8409471854:AAGhK3UE65tnBTZIWguPS2_4fBI1Yed4VM8",
+  bot_token: "",
   timezone: "Europe/Moscow"
 
 # Настройки библиотеки Telegex
 config :telegex,
-  token: "8409471854:AAGhK3UE65tnBTZIWguPS2_4fBI1Yed4VM8",
+  token: "",
   adapter: {Telegex.Adapter.Finch, name: TimeBot.Finch}
