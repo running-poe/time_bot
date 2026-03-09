ed27afbd1c46e9e91f7ccc680a50f09926b76a49
running-poe <djunkovsky.p@gmail.com>
2026-03-09 19:48:45 +0300

    отказ от буррито

--- mix.exs
+++ mix.exs
@@ -7,21 +7,7 @@ defmodule TimeBot.MixProject do
     version: "0.1.0",
     elixir: "~> 1.14",
     start_permanent: Mix.env() == :prod,
-    deps: deps(),
-    # Настройка релизов
-    releases: [
-      time_bot: [
-        steps: [:assemble, &Burrito.wrap/1], # Включаем Burrito
-        burrito: [
-          targets: [
-            # Цель для Raspberry Pi (32-bit или 64-bit)
-            # Если ваша "Малина" 32-bit (стандартная): linux_arm
-            # Если 64-bit: linux_arm64
-            raspberry: [os: :linux, cpu: :arm]
-          ]
-        ]
-      ]
-    ]
+    deps: deps()
   ]
 end
 
@@ -36,8 +22,7 @@ end
     [
       {:telegex, "~> 1.7"},
       {:timex, "~> 3.7"},
-      {:finch, "~> 0.16"},
-      {:burrito, "~> 1.0"}
+      {:finch, "~> 0.16"}
     ]
   end
 end
